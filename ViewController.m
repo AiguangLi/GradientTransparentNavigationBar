7372570da3ddcfddac9a4c52e5ebdafe5b13d0ba
WaterSword <Aiguang.Li@gmail.com>
2016-04-19 18:02:19 +0800

    Add demo projects

---
+++ ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GradientTransparentNavigationBar
+//
+//  Created by kimi on 16/4/19.
+//  Copyright © 2016年 mooeen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
